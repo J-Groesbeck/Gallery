203d42112df6d2ddd28fb7b08fc5aeb38c0cde93
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-11-07 13:18:25 -0500

    Javascript Testing Complete

---
+++ script.js
@@ -0,0 +1,95 @@
+function monthglowNov() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    November.style.display = "none";
+}
+
+function monthglowDec() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    December.style.display = "none";
+}
+
+function monthglowJan() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    January.style.display = "none";
+}
+
+function monthglowFeb() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    February.style.display = "none";
+}
+
+function monthglowMar() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    March.style.display = "none";
+}
+
+function monthglowApr() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    April.style.display = "none";
+}
+
+function monthglowMay() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    May.style.display = "none";
+}
+
+function monthglowJun() {
+    var November = document.getElementById("Nov");
+    var December = document.getElementById("Dec");
+    var January = document.getElementById("Jan");
+    var February = document.getElementById("Feb");
+    var March = document.getElementById("Mar");
+    var April = document.getElementById("Apr");
+    var May = document.getElementById("May");
+    var June = document.getElementById("Jun");
+    June.style.display = "none";
+}
