cb5e4a9ca78ba805cf16a7286b895a90e13d3fac
J-Groesbeck <144719158+J-Groesbeck@users.noreply.github.com>
2023-11-08 15:12:47 +0000

    Scroll to Top Button for Mobile (untested)

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//Scroll top button functionality for mobile
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Add shadow to card associated with nav button
 function monthglowNov() {
     var November = document.getElementById("Nov");
     var December = document.getElementById("Dec");
